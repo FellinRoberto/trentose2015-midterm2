47fe5efb8b2fbac729f76d7df86f1ae873183b98
Roberto <fellin.roberto@hotmail.it>
2015-11-05 10:24:14 +0100

    fine esame

--- app.js
+++ app.js
@@ -10,18 +10,78 @@
  */ 
 
 var tmpl = ' <li id="ID">' +
-           '  <h3>WORD</h3>' +
-           '  <h3 class="solution">SOLUTION</h3>'+
-           ' </li> ';
+        '  <h3>WORD</h3>' +
+        '  <h3 class="solution">SOLUTION</h3>'+
+        ' </li> ';
 
 $(document).ready(function(){
+    
+    var tot=0;
+    $(".options").hide();
+    $(".final").hide();
+    
+    for(var i=0;i<data.length;i++){
+        
+        var tmp=tmpl.replace("ID",i).replace("WORD",data[i].word_en).replace("SOLUTION",data[i].word_de);
+        $(".cards").append(tmp);
+        
+    }
+    
+    $("#0").addClass("current");
+    $(".solution").hide();
+    
+    $( ".cards" ).on( "click", "li", function() { 
+        $(this).children().hide();
+	$(".solution").show();
+        $(".options").show();
+    });
+
+    $(".opt-correct").click(function(){
+        tot+=1;
+        var c=$(".current").attr("id");
+        $("#"+c).removeClass("current");
+        var c2=parseInt(c)+1;
+       
+        if(c2>=data.length){
+            console.log("fine");
+            $(".solution").hide();
+            $(".options").hide();
+           
+            $("#tot").text(data.length);
+            $("#tot-good").text(tot);
+            $(".final").show();
+           
+            $(".final").show();
+        }else{
+            $("#"+c2).addClass("current");
+            $(".solution").hide();
+            $(".options").hide();}
+        console.log(c2);
+    });
+
+    $(".opt-incorrect").click(function(){
+       
+        var c=$(".current").attr("id");
+        $("#"+c).removeClass("current");
+        var c2=parseInt(c)+1;
+        if(c2>=data.length){
+            console.log("fine");
+            $(".solution").hide();
+            $(".options").hide();
+           
+            $("#tot").text(data.length);
+            $("#tot-good").text(tot);
+            $(".final").show();
+        }else{
+            $("#"+c2).addClass("current");
+            $(".solution").hide();
+            $(".options").hide();
+            console.log(c2);
+        }
+    });
+
+
 
 
 });
-
-
-
-
-
-
 
